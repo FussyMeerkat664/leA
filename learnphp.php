3667f1d2b4fc7e727e80c702caee6b34f33a6b21
FussyMeerkat664 <james.hl@oundleschool.org.uk>
2022-10-11 10:29:41 +0100

    test

---
+++ learnphp.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        echo "My first PHP script!";
+        ?>
+        <?php
+        $txt = "PHP";
+        echo "I love $txt!";
+        ?>
+    </body>
+</html>
